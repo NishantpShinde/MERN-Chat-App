d9cffe57b840ebe570ae1779eb59dac48b5d83c0
Nishant Shinde <imnishant333@gmail.com>
2024-08-06 20:51:36 +0530

    Updated Socket.IO connection URL

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://talk-a-tive-5dtb.onrender.com";
 
 const ChatContext = createContext();
 
